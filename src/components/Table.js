2025f3109fd8d2c03bf53ee745ac98b37f66d7ca
Arkadiusz Konopka <arekcommerce@gmail.com>
2021-05-01 15:04:58 +0200

    add sorting

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,6 @@
 import mockData from '../asset/MOCK_DATA.json'
 import COLUMNS from './columns'
-import { useTable } from 'react-table'
+import { useTable, useSortBy } from 'react-table'
 import { useMemo } from 'react'
 
 
@@ -18,7 +18,8 @@ const Table = () => {
     {
       columns,
       data
-    }
+    },
+    useSortBy
   )
 
   return <div>
@@ -27,7 +28,11 @@ const Table = () => {
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render("Header")}
+                {column.isSorted ? column.isSortedDesc ? <i class="fas fa-sort-up"></i> : <i class="fas fa-sort-down"></i> : ""}
+                {console.log(column)}
+              </th>
             ))}
           </tr>
         ))}
